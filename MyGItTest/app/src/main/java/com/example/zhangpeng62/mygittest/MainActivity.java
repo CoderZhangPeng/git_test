eeaf0340632a62e4895c7933ca3940212318caf6
张鹏62 <zhangpeng62@hikvision.com>
2019-11-26 18:32:04 +0800

    修改mainActivity

--- MyGItTest/app/src/main/java/com/example/zhangpeng62/mygittest/MainActivity.java
+++ MyGItTest/app/src/main/java/com/example/zhangpeng62/mygittest/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.zhangpeng62.mygittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = (TextView) findViewById(R.id.test);
+        test.setText("test");
     }
 }
