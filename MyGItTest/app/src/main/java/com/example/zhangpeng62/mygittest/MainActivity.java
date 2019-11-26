d8688da4fa9ad8ce64d327eff6b77d2e7ed91350
张鹏62 <zhangpeng62@hikvision.com>
2019-11-26 20:27:08 +0800

    测试 git add .

--- MyGItTest/app/src/main/java/com/example/zhangpeng62/mygittest/MainActivity.java
+++ MyGItTest/app/src/main/java/com/example/zhangpeng62/mygittest/MainActivity.java
@@ -15,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         test = (TextView) findViewById(R.id.test);
         test.setText("test");
+
         test.setTextColor(Color.GREEN);
+
         test.setTextSize(14);
     }
 }
